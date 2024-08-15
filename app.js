0613872b872145b7a8cd2040698657aceed7e0ae
cnktass <cnktass@gmail.com>
2024-08-15 10:24:52 +0300

    ejs cevirme ve active yapisini saglama

--- app.js
+++ app.js
@@ -1,10 +1,24 @@
 const express = require('express');
 
 const app = express();
+//TEMPLATE ENGINE
+app.set("view engine", "ejs")
 
+//Middlewares
+app.use(express.static("public"))
+
+//Routes
 app.get('/', (req, res) => {
-  res.status(200).send('INDEX SAYFASI');
-});
+    res.status(200).render('index', {
+        page_name: "index"
+    });
+  });
+
+  app.get('/about', (req, res) => {
+    res.status(200).render('about', {
+      page_name: 'about',
+    });
+  })
 
 const port = 3000;
 app.listen(port, () => {
