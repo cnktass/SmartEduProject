aef212b0531fae6f3ce1a5ae74caa90e33f85d5b
cnktass <cnktass@gmail.com>
2024-08-15 15:20:27 +0300

    controller islemleri

--- app.js
+++ app.js
@@ -1,24 +1,27 @@
 const express = require('express');
+const pageRoute = require('./routes/pageRoute')
+const courseRoute = require('./routes/courseRoute')
+const mongoose = require('mongoose')
 
 const app = express();
+//Connect DB
+ mongoose.connect('mongodb://127.0.0.1/smartedu-db');
+
 //TEMPLATE ENGINE
 app.set("view engine", "ejs")
 
 //Middlewares
 app.use(express.static("public"))
+app.use(express.json())
+app.use(express.urlencoded({
+  extended:true
+}))
 
 //Routes
-app.get('/', (req, res) => {
-    res.status(200).render('index', {
-        page_name: "index"
-    });
-  });
+app.use('/', pageRoute);
+app.use('/courses', courseRoute);
 
-  app.get('/about', (req, res) => {
-    res.status(200).render('about', {
-      page_name: 'about',
-    });
-  })
+
 
 const port = 3000;
 app.listen(port, () => {
